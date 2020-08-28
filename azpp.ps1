aaa3b63abf8026aeb5852ab40699ae3af228a48c
ceceteras <info.durucynthia@gmail.com>
2020-08-28 08:33:04 +0100

    Updated powershell script

--- azpp.ps1
+++ azpp.ps1
@@ -1,40 +1,22 @@
-# First, we create our variables
- 
+# First, we create our variables 
 $location = EastUS
 $resourceGroup = Az-powerpuff 
 $virtualMachineName = Az-powerpuff-VM
 $virtualnetwork = Az-powerpuff-vnet
 $Cred = Get-Credential -Message "Enter a username and password for the virtual machine."
 
- 
-
- 
 
 # Create resource group
-
- 
-
 New-AzResourceGroup -ResourceGroupName $resourceGroup -Location $location
 
- 
-
 # Create public IP address
-
- 
-
- $pip = New-AzPublicIpAddress `
+$pip = New-AzPublicIpAddress `
           -ResourceGroupName $resourceGroup `
           -Location $location `
           -AllocationMethod Dynamic `
           -Name myPublicIPAddress
 
- 
-
-
 # create frontend and backend subnets
-
- 
-
 $AzpowerpuffFrontendSubnet = New-AzVirtualNetworkSubnetConfig `
                         -Name AzpowerpuffFrontendSubnet `
                         -AddressPrefix 10.0.0.0/24
@@ -42,12 +24,8 @@ $AzpowerpuffBackendSubnet = New-AzVirtualNetworkSubnetConfig `
                         -Name AzpowerpuffBackendSubnet `
                         -AddressPrefix 10.0.1.0/24
 
- 
 
 # Create a virtual network
-
- 
-
 $vnet = New-AzVirtualNetwork `
         -ResourceGroupName $resourceGroup`
         -Location $location `
@@ -55,13 +33,7 @@ $vnet = New-AzVirtualNetwork `
         -AddressPrefix 10.0.0.0/16 `
         -Subnet $AzpowerpuffFrontendSubnet, $AzpowerpuffBackendSubnet
 
- 
-
-
 # Create virtual network interface card
-
- 
-
 $frontendNic = New-AzNetworkInterface `
                     -ResourceGroupName $resourceGroup `
                     -Location $location `
@@ -69,29 +41,16 @@ $frontendNic = New-AzNetworkInterface `
                     -SubnetId $vnet.Subnets[0].Id `
                     -PublicIpAddressId $pip.Id
 
- 
-
-
 # Create a front end Virtual machine
-
- 
-
 $vmConfig = New-AzVMConfig -VMName $virtualMachineName -VMSize 'Standard B2s' | `
   Set-AzVMOperatingSystem -Windows -ComputerName 'powerpuffVm-Web' -Credential $cred | `
   Set-AzVMSourceImage -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' `
   -Skus '2016-Datacenter' -Version latest | Add-AzVMNetworkInterface -Id $nicVMweb.Id
 
  
-
  New-AzVM -ResourceGroupName $resourceGroup -Location $location -VM $vmConfig
 
- 
-
-
 # Create an inbound rule named myFrontendNSGRule to allow icoming web traffic on myFrontendVM 
-
- 
-
 $nsgFrontendRule = New-AzNetworkSecurityRuleConfig `
   -Name myFrontendNSGRule `
   -Protocol Tcp `
@@ -105,11 +64,8 @@ $nsgFrontendRule = New-AzNetworkSecurityRuleConfig `
 
  
 
-  # You can limit internal traffic to myBackendVM from only myFrontendVM by creating an NSG for the back-end subnet. 
-
- 
-
-  $nsgBackendRule = New-AzNetworkSecurityRuleConfig `
+# You can limit internal traffic to myBackendVM from only myFrontendVM by creating an NSG for the back-end subnet. 
+$nsgBackendRule = New-AzNetworkSecurityRuleConfig `
   -Name myBackendNSGRule `
   -Protocol Tcp `
   -Direction Inbound `
@@ -120,50 +76,39 @@ $nsgFrontendRule = New-AzNetworkSecurityRuleConfig `
   -DestinationPortRange 1433 `
   -Access Allow
 
- 
 
-  # Add a network security group named myFrontendNSG
-
- 
-
-  $nsgFrontend = New-AzNetworkSecurityGroup `
+# Add a network security group named myFrontendNSG
+$nsgFrontend = New-AzNetworkSecurityGroup `
   -ResourceGroupName myRGNetwork `
   -Location EastUS `
   -Name myFrontendNSG `
   -SecurityRules $nsgFrontendRule
 
- 
-
-  # Add a network security group named myBackendNSG
-
- 
-
-  $nsgBackend = New-AzNetworkSecurityGroup `
+# Add a network security group named myBackendNSG
+$nsgBackend = New-AzNetworkSecurityGroup `
   -ResourceGroupName myRGNetwork `
   -Location EastUS `
   -Name myBackendNSG `
   -SecurityRules $nsgBackendRule
 
  
-
-  # Add the network security groups to the subnets
-
- 
-
-  $vnet = Get-AzVirtualNetwork `
+# Add the network security groups to the subnets
+$vnet = Get-AzVirtualNetwork `
   -ResourceGroupName myRGNetwork `
   -Name myVNet
 $frontendSubnet = $vnet.Subnets[0]
 $backendSubnet = $vnet.Subnets[1]
+
 $frontendSubnetConfig = Set-AzVirtualNetworkSubnetConfig `
   -VirtualNetwork $vnet `
   -Name myFrontendSubnet `
-  -AddressPrefix $frontendSubnet.AddressPrefix `
+  -AddressPrefix $AzpowerpuffFrontendSubnet.AddressPrefix `
   -NetworkSecurityGroup $nsgFrontend
+
 $backendSubnetConfig = Set-AzVirtualNetworkSubnetConfig `
   -VirtualNetwork $vnet `
   -Name myBackendSubnet `
-  -AddressPrefix $backendSubnet.AddressPrefix `
+  -AddressPrefix $AzpowerpuffBackendSubnet.AddressPrefix `
   -NetworkSecurityGroup $nsgBackend
 Set-AzVirtualNetwork -VirtualNetwork $vnet
 
